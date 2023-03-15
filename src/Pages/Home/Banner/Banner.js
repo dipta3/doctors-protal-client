e2ffd42f0ff3e48eb406021a08259257142af978
Dipta Biswas <diptabiswas79@gmail.com>
2023-03-15 21:53:05 +0600

    Home Page Responsiveness fixed

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,7 +3,7 @@ import chair from '../../../assets/images/chair.png'
 import PrimayButton from '../../../components/PrimaryButton/PrimayButton';
 const Banner = () => {
     return (
-        <div className="hero h-96 bg-[url('/src/assets/images/bg.png')] mb-10">
+        <div className="hero bg-[url('/src/assets/images/bg.png')]">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <img alt='' src={chair} className="rounded-lg lg:w-1/2 shadow-2xl" />
                 <div>
--- src/Pages/Home/ServiceCards/ServiceCards.js
+++ src/Pages/Home/ServiceCards/ServiceCards.js
@@ -41,9 +41,9 @@ const ServiceCards = () => {
                 }
             </div>
             <div>
-                <div className="hero min-h-screen">
+                <div className="hero min-h-screen mt-14">
                     <div className="hero-content flex-col lg:flex-row">
-                        <img src={treatment} alt='' width={'350'} className="max-w-sm rounded-lg shadow-2xl lg:w-1/2" />
+                        <img src={treatment} alt='' width={'350'} className="lg:max-w-xs rounded-lg shadow-2xl lg:w-1/2" />
                         <div className='lg:w-1/2 lg:px-10'>
                             <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
                             <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
