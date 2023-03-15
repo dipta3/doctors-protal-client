cd8cde4144229075185d5589b6d91302358a861f
Dipta Biswas <diptabiswas79@gmail.com>
2023-03-15 21:43:46 +0600

    footer added

--- src/Pages/Home/ContractUs/ContractUs.js
+++ src/Pages/Home/ContractUs/ContractUs.js
@@ -11,7 +11,7 @@ const ContractUs = () => {
             <div className='py-20'>
                 <h3 className='text-secondary font-bold text-xl '>Contract Us</h3>
                 <h4 className='text-white mt-3 text-4xl'>Stay connected with us</h4>
-                <div className=''>
+                <div className='px-5'>
                     <input type="text" placeholder="Type here" className="input mt-10 input-bordered input-md w-full max-w-xl" /><br />
                     <input type="text" placeholder="Type here" className="input mt-5 input-bordered input-md w-full max-w-xl" /><br />
                     <textarea placeholder="Bio" className="textarea textarea-bordered mt-5 mb-9 h-32 textarea-md w-full max-w-xl" ></textarea>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,34 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import footer from '../../../assets/images/footer.png'
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer</h2>
-        </div>
+        <footer className=" mt-10 p-10 " style={{
+            background: `url(${footer})`,
+            backgroundSize: 'cover'
+        }}>
+            <div className='footer px-12'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to='/' className="link link-hover">Emergency Checkup</Link>
+                    <Link to='/' className="link link-hover">Monthly Checkup</Link>
+                    <Link to='/' className="link link-hover">Weekly Checkup</Link>
+                    <Link to='/' className="link link-hover">Deep Checkup</Link>
+                </div>
+                <div>
+                    <span className="footer-title">ORAL HEALTH</span>
+                    <Link to='/' className="link link-hover">Fluoride Treatment</Link>
+                    <Link to='/' className="link link-hover">Cavity Filling</Link>
+                    <Link to='/' className="link link-hover">Teath Whitening</Link>
+
+                </div>
+                <div>
+                    <span className="footer-title">Address</span>
+                    <p>Ahamednagar Mirpur-1</p>
+                </div>
+            </div>
+            <p className='text-center mt-32'>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+        </footer >
     );
 };
 
