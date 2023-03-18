3059126f8b44b37a61c14525edf9d37f9195a292
Dipta Biswas <diptabiswas79@gmail.com>
2023-03-18 15:54:02 +0600

    Implement Logout

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
@@ -13,7 +13,9 @@ const AuthProvider = ({ children }) => {
     const signIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password)
     }
-
+    const logOut = () => {
+        return signOut(auth)
+    }
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
@@ -24,7 +26,8 @@ const AuthProvider = ({ children }) => {
     const authInfo = {
         user,
         createUser,
-        signIn
+        signIn,
+        logOut
 
     }
     return (
