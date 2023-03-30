43505ea51c59cfeb9e88b39da99708e51ce17f27
Dipta Biswas <diptabiswas79@gmail.com>
2023-03-30 21:07:14 +0600

    Reamefile edited

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -11,10 +11,7 @@ const Navbar = () => {
     }
     const menuItems = <React.Fragment>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
         <li><Link to='/appointment'>Appointment</Link></li>
-
-        <li><Link to='/contract'>Contact Us</Link></li>
         {
             user?.uid ?
                 <>
